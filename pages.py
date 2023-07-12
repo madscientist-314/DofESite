5c4aaae4252054b310d150a026d672149846be58
madscientist-314 <78171604+madscientist-314@users.noreply.github.com>
2023-07-12 17:34:18 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    .prettierignore
    	deleted:    .replit
    	modified:   pages.py
    	new file:   pages/bronze.html
    	new file:   pages/gold.html
    	modified:   pages/index.html
    	new file:   pages/silver.html
    	deleted:    replit.nix
    	deleted:    script.js

--- pages.py
+++ pages.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from flask import Flask, render_template, send_file
+from flask import Flask, send_file
 
 app = Flask(
   __name__,
@@ -16,48 +16,52 @@ def runwebsite():
 
 @app.route('/')
 def home():
-    return send_file('DukeOfEdinburgh.html')
+    return send_file('pages/index.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return send_file('Error.html')
+    return send_file('pages/error.html')
 
 @app.route('/bronze')
 def bronze():
-    return send_file('Bronze.html')
+    return send_file('pages/bronze.html')
 
 @app.route('/bronze/expedition')
 def bronze_exped():
-    return send_file('BExpedition.html')
+    return send_file('pages/bexpedition.html')
 
 @app.route('/bronze/skill')
 def bronze_skill():
-    return send_file('BSkill.html')
+    return send_file('pages/bskill.html')
 
 @app.route('/bronze/volunteering')
 def bronze_volunteer():
-    return send_file('BVolunteering.html')
+    return send_file('pages/bvolunteering.html')
 
 @app.route('/bronze/physical')
 def bronze_physical():
-    return send_file('BPhysical.html')
+    return send_file('pages/bphysical.html')
 
 @app.route('/silver')
 def silver():
-    return send_file('Silver.html')
+    return send_file('pages/silver.html')
 
 @app.route('/silver/expedition')
 def silver_exped():
-    return send_file('SExpedition.html')
+    return send_file('pages/sexpedition.html')
 
 @app.route('/silver/skill')
 def silver_skill():
-    return send_file('SSkill.html')
+    return send_file('pages/sskill.html')
 
 @app.route('/silver/volunteering')
 def silver_volunteer():
-    return send_file('SVolunteering.html')
+    return send_file('pages/svolunteering.html')
 
 @app.route('/silver/physical')
 def silver_physical():
-    return send_file('SPhysical.html')
+    return send_file('pages/sphysical.html')
+
+@app.route('/gold')
+def gold():
+  return send_file('pages/gold.html')
